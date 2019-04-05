7bab4f509332595abbb3e272aeb228fa8006f268
Edward Griffith <egriffit@localhost.localdomain>
2019-04-05 17:44:42 -0400

    Changed all indents to four spaces

--- test/testBoard.cpp
+++ test/testBoard.cpp
@@ -1,167 +1,167 @@
-#include "catch.hpp"	//CATCH testing framework
+#include "catch.hpp"    //CATCH testing framework
 #include "../src/Board.h"
 #include <cstdlib>
 #include <iostream>
 
 TEST_CASE("SudokuBoard constructors are called", "[constructor]")
 {
-	//a test board and default board
-	std::vector<int> testBoard(81, 7);
-	std::vector<int> defaultBoard(81, -1);
-	
-	
-	//call each constructor
-	Board A;			//Default Constructor
-	Board B(testBoard);	//Second Constructor
-	Board C(B);			//Copy Constructor
-	
+    //a test board and default board
+    std::vector<int> testBoard(81, 7);
+    std::vector<int> defaultBoard(81, -1);
+    
+    
+    //call each constructor
+    Board A;            //Default Constructor
+    Board B(testBoard); //Second Constructor
+    Board C(B);         //Copy Constructor
+    
 
-	//Assertions
-	REQUIRE( A.getBoard() == defaultBoard );
-	REQUIRE( B.getBoard() == testBoard );
-	REQUIRE( C == B );
+    //Assertions
+    REQUIRE( A.getBoard() == defaultBoard );
+    REQUIRE( B.getBoard() == testBoard );
+    REQUIRE( C == B );
 }
 
 TEST_CASE("Accessors are called", "[accessor]")
 {
-	//test board
-	std::vector<int> testBoard(81, -1);
+    //test board
+    std::vector<int> testBoard(81, -1);
 
-	//fill in cells with row count
-	for(int i = 0; i < 9; i++)
-		for(int j = 0; j < 9; j++)
-			testBoard[(i * 9) + j] = i;
-	
-	
-	//row to be compared to
-	std::vector<int> fifthRow(9, 4);
+    //fill in cells with row count
+    for(int i = 0; i < 9; i++)
+        for(int j = 0; j < 9; j++)
+            testBoard[(i * 9) + j] = i;
+    
+    
+    //row to be compared to
+    std::vector<int> fifthRow(9, 4);
 
-	//column to be compared to
-	std::vector<int> fourthCol;
-	for(int i = 0; i < 9; i++)
-		fourthCol.push_back(i);
+    //column to be compared to
+    std::vector<int> fourthCol;
+    for(int i = 0; i < 9; i++)
+        fourthCol.push_back(i);
 
-	//block to compare to
-	std::vector<int> sixthBlock;
-	for(int i = 3; i < 6; i++)
-	{
-		sixthBlock.push_back(i);
-		sixthBlock.push_back(i);
-		sixthBlock.push_back(i);
-	}
+    //block to compare to
+    std::vector<int> sixthBlock;
+    for(int i = 3; i < 6; i++)
+    {
+        sixthBlock.push_back(i);
+        sixthBlock.push_back(i);
+        sixthBlock.push_back(i);
+    }
 
-	//Board
-	Board A(testBoard);
+    //Board
+    Board A(testBoard);
 
-	//Assertions
-	REQUIRE( A.getRow(4) == fifthRow);
-	REQUIRE( A.getCol(3) == fourthCol);
-	REQUIRE( A.getCell(3, 3) == 3);
-	REQUIRE( A.getBlock(5) == sixthBlock);
-	
+    //Assertions
+    REQUIRE( A.getRow(4) == fifthRow);
+    REQUIRE( A.getCol(3) == fourthCol);
+    REQUIRE( A.getCell(3, 3) == 3);
+    REQUIRE( A.getBlock(5) == sixthBlock);
+    
 }
 
 TEST_CASE("Mutators are called", "[mutator]")
 {
-	//test board
-	std::vector<int> testBoard(81, -1);
-	Board A(testBoard);
-	
-	//changed rows, cols, and cells
-	std::vector<int> row(9, 4);
-	std::vector<int> col(9, 6);
-	std::vector<int> block(9, 9);
+    //test board
+    std::vector<int> testBoard(81, -1);
+    Board A(testBoard);
+    
+    //changed rows, cols, and cells
+    std::vector<int> row(9, 4);
+    std::vector<int> col(9, 6);
+    std::vector<int> block(9, 9);
 
-	//check getFilled = 0
-	REQUIRE( A.getFilled() == 0);
-	
+    //check getFilled = 0
+    REQUIRE( A.getFilled() == 0);
+    
 
 
-	//check setCell()
-	A.setCell(0, 0, 9);
-	REQUIRE( A.getCell(0, 0) == 9 );
-	
-	//check setRow()
-	A.setRow(5, row);
-	REQUIRE( A.getRow(5) == row );
-	
-	//check setCol()
-	A.setCol(8, col);
-	REQUIRE( A.getCol(8) == col );
-	
-	//check setBlock()
-	A.setBlock(6, block);
-	REQUIRE( A.getBlock(6) == block);
+    //check setCell()
+    A.setCell(0, 0, 9);
+    REQUIRE( A.getCell(0, 0) == 9 );
+    
+    //check setRow()
+    A.setRow(5, row);
+    REQUIRE( A.getRow(5) == row );
+    
+    //check setCol()
+    A.setCol(8, col);
+    REQUIRE( A.getCol(8) == col );
+    
+    //check setBlock()
+    A.setBlock(6, block);
+    REQUIRE( A.getBlock(6) == block);
 
-	//check filled again
-	REQUIRE( A.getFilled() == 27);
+    //check filled again
+    REQUIRE( A.getFilled() == 27);
 
 }
 
 TEST_CASE("Check for the board being filled", "[fullboard]")
 {
-	//test board
-	std::vector<int> testBoard(81, 9);
-	Board A(testBoard);
+    //test board
+    std::vector<int> testBoard(81, 9);
+    Board A(testBoard);
 
-	//check full
-	REQUIRE( A.getFilled() == 81 );
-	REQUIRE( A.getRowFilled(0) == 9 );
-	REQUIRE( A.getColFilled(0) == 9 );
-	REQUIRE( A.getRowEmpty(0) == 0 );
-	REQUIRE( A.getColEmpty(0) == 0 );
-	REQUIRE( A.isRowFull(0) == true );
-	REQUIRE( A.isColFull(0) == true );
-	REQUIRE( A.isFull() == true );
+    //check full
+    REQUIRE( A.getFilled() == 81 );
+    REQUIRE( A.getRowFilled(0) == 9 );
+    REQUIRE( A.getColFilled(0) == 9 );
+    REQUIRE( A.getRowEmpty(0) == 0 );
+    REQUIRE( A.getColEmpty(0) == 0 );
+    REQUIRE( A.isRowFull(0) == true );
+    REQUIRE( A.isColFull(0) == true );
+    REQUIRE( A.isFull() == true );
 
-	//change one value to empty
-	A.setCell(0, 0, -1);
+    //change one value to empty
+    A.setCell(0, 0, -1);
 
-	//check for change
-	REQUIRE( A.getFilled() == 80 );
-	REQUIRE( A.getRowFilled(0) == 8 );
-	REQUIRE( A.getColFilled(0) == 8 );
-	REQUIRE( A.getRowEmpty(0) == 1 );
-	REQUIRE( A.getColEmpty(0) == 1 );
-	REQUIRE( A.isRowFull(0) == false );
-	REQUIRE( A.isColFull(0) == false );
-	REQUIRE( A.isFull() == false );
+    //check for change
+    REQUIRE( A.getFilled() == 80 );
+    REQUIRE( A.getRowFilled(0) == 8 );
+    REQUIRE( A.getColFilled(0) == 8 );
+    REQUIRE( A.getRowEmpty(0) == 1 );
+    REQUIRE( A.getColEmpty(0) == 1 );
+    REQUIRE( A.isRowFull(0) == false );
+    REQUIRE( A.isColFull(0) == false );
+    REQUIRE( A.isFull() == false );
 }
 
 TEST_CASE("Comparison operators are called", "[compare]")
 {
-	Board A;
-	Board B;
-	
+    Board A;
+    Board B;
+    
 
-	//boards should be equal
-	REQUIRE( B == A );
+    //boards should be equal
+    REQUIRE( B == A );
 
-	A.setCell(0, 0, 9);
-	B.setCell(0, 0, 9);
+    A.setCell(0, 0, 9);
+    B.setCell(0, 0, 9);
 
-	REQUIRE( B == A );
-	REQUIRE( (B < A) == false );
-	REQUIRE( (A < B) == false );
+    REQUIRE( B == A );
+    REQUIRE( (B < A) == false );
+    REQUIRE( (A < B) == false );
 
-	B.setCell(1, 1, 9);
+    B.setCell(1, 1, 9);
 
-	REQUIRE( (B == A) == false );
-	REQUIRE( (A < B) == true );
-	REQUIRE( (B < A) == false );
+    REQUIRE( (B == A) == false );
+    REQUIRE( (A < B) == true );
+    REQUIRE( (B < A) == false );
 }
 
 TEST_CASE("Assignment operator is used", "[assignment]")
 {
-	//test board
-	std::vector<int> testBoard;
-	for(int i = 0; i < 81; i++)
-		testBoard.push_back(i);
+    //test board
+    std::vector<int> testBoard;
+    for(int i = 0; i < 81; i++)
+        testBoard.push_back(i);
 
-	Board A(testBoard);
-	Board B = A;
+    Board A(testBoard);
+    Board B = A;
 
-	REQUIRE( A == B );
+    REQUIRE( A == B );
 
 }
 
--- test/testSolver.cpp
+++ test/testSolver.cpp
@@ -1,4 +1,4 @@
-#include "catch.hpp"	//CATCH testing framework
+#include "catch.hpp"    //CATCH testing framework
 #include "../src/SudokuSolver.h"
 #include "../src/Board.h"
 #include <cstdlib>
@@ -8,113 +8,113 @@
 
 TEST_CASE("SudokuSolver constructors are called", "[constructor]")
 {
-	
-	//default
-	SudokuSolver A;	
-	std::vector<bool> c(9, false);
+    
+    //default
+    SudokuSolver A; 
+    std::vector<bool> c(9, false);
 
-	REQUIRE( A.board.getFilled() == 0 );
-	REQUIRE( A.getChecker() == c );
-	
-	
-	//alternate constructor
-	std::vector<int> testBoard(81, 0);
-	Board test(testBoard);	
-	SudokuSolver B(test);
-	REQUIRE( B.board == test );
+    REQUIRE( A.board.getFilled() == 0 );
+    REQUIRE( A.getChecker() == c );
+    
+    
+    //alternate constructor
+    std::vector<int> testBoard(81, 0);
+    Board test(testBoard);  
+    SudokuSolver B(test);
+    REQUIRE( B.board == test );
 
 
-	//copy constructor
-	SudokuSolver C(B);
-	REQUIRE( C == B );
-	
+    //copy constructor
+    SudokuSolver C(B);
+    REQUIRE( C == B );
+    
 }
 
 TEST_CASE("SudokuSolver accessors are called", "[accessor]")
 {
-	SudokuSolver A;
-	std::vector<bool> c(9, false);
+    SudokuSolver A;
+    std::vector<bool> c(9, false);
 
-	REQUIRE( A.getChecker() == c );
+    REQUIRE( A.getChecker() == c );
 }
 
 TEST_CASE("SudokuSolver mutators are called", "[mutator]")
 {
-	SudokuSolver A;
-	std::vector<bool> newChecker(9, true);
-	std::vector<bool> allFalse(9, false);
+    SudokuSolver A;
+    std::vector<bool> newChecker(9, true);
+    std::vector<bool> allFalse(9, false);
 
 
-	//set checker
-	A.setChecker(newChecker);
-	REQUIRE( A.getChecker() == newChecker );
+    //set checker
+    A.setChecker(newChecker);
+    REQUIRE( A.getChecker() == newChecker );
 
 
-	//reset checker
-	A.resetChecker();
-	REQUIRE( A.getChecker() == allFalse );
+    //reset checker
+    A.resetChecker();
+    REQUIRE( A.getChecker() == allFalse );
 }
 
 TEST_CASE("Solve a row/column/block with 1 empty space", "[solving]")
 {
-	std::vector<int> test(9, -1);
-	for(int i = 0; i < 9; i++)
-		test[i] = i + 1;
-	std::vector<int> solvedTest = test;
-	test[5] = -1;
+    std::vector<int> test(9, -1);
+    for(int i = 0; i < 9; i++)
+        test[i] = i + 1;
+    std::vector<int> solvedTest = test;
+    test[5] = -1;
 
 
-	//solve row
-	SudokuSolver A;
-	A.board.setRow(3, test);
-		
-	A.solve8Full(3, 'r');
-	REQUIRE( A.board.getRow(3) == solvedTest );
+    //solve row
+    SudokuSolver A;
+    A.board.setRow(3, test);
+        
+    A.solve8Full(3, 'r');
+    REQUIRE( A.board.getRow(3) == solvedTest );
 
 
-	//solve column
-	SudokuSolver B;
-	B.board.setCol(2, test);
+    //solve column
+    SudokuSolver B;
+    B.board.setCol(2, test);
 
-	B.solve8Full(2, 'c');
-	REQUIRE( B.board.getCol(2) == solvedTest );
+    B.solve8Full(2, 'c');
+    REQUIRE( B.board.getCol(2) == solvedTest );
 
 
-	//solve block
-	SudokuSolver C;
-	C.board.setBlock(8, test);
+    //solve block
+    SudokuSolver C;
+    C.board.setBlock(8, test);
 
-	C.solve8Full(8, 'b');
-	REQUIRE( C.board.getBlock(8) == solvedTest );
+    C.solve8Full(8, 'b');
+    REQUIRE( C.board.getBlock(8) == solvedTest );
 }
 
 
 TEST_CASE("Driver for the solver is called", "[solving]")
 {
-	std::vector<int> testBoard = {4, 2, 3, 6, 9, 7, 8, 1, 5,
-								  6, 9, 1, 5, 3, 8, 5, 7, 2,
-								  5, 8, 7, 4, 2, 1, 6, 3, 9,
-								  3, 1, 9, 8, 7, 5, 2, 6, 4,
-								  2, 5, 6, 1, 4, 9, 3, 8, 7,
-								  7, 4, 8, 3, 6, 2, 5, 9, 1,
-								  9, 6, 4, 2, 1, 3, 7, 5, 8,
-								  1, 3, 5, 7, 8, 4, 9, 2, 6,
-								  8, 7, 2, 9, 5, 6, 1, 4, 3};
-	
-	std::vector<int> solvedBoard = testBoard;
+    std::vector<int> testBoard = {4, 2, 3, 6, 9, 7, 8, 1, 5,
+                                  6, 9, 1, 5, 3, 8, 5, 7, 2,
+                                  5, 8, 7, 4, 2, 1, 6, 3, 9,
+                                  3, 1, 9, 8, 7, 5, 2, 6, 4,
+                                  2, 5, 6, 1, 4, 9, 3, 8, 7,
+                                  7, 4, 8, 3, 6, 2, 5, 9, 1,
+                                  9, 6, 4, 2, 1, 3, 7, 5, 8,
+                                  1, 3, 5, 7, 8, 4, 9, 2, 6,
+                                  8, 7, 2, 9, 5, 6, 1, 4, 3};
+    
+    std::vector<int> solvedBoard = testBoard;
 
-	//remove certain values of the board
-	testBoard[6] = -1;
-	testBoard[55] = -1;
-	testBoard[23] = -1;
+    //remove certain values of the board
+    testBoard[6] = -1;
+    testBoard[55] = -1;
+    testBoard[23] = -1;
 
-	Board tester(testBoard);
-	Board solved(solvedBoard);
+    Board tester(testBoard);
+    Board solved(solvedBoard);
 
 
-	SudokuSolver A(tester);
-	A.solveDriver();
-	
-	REQUIRE( A.board == solved );
+    SudokuSolver A(tester);
+    A.solveDriver();
+    
+    REQUIRE( A.board == solved );
 }
 
